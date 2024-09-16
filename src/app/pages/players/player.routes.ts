377e94957bb008350cd2138c75b8f5dcb44f7c26
aledguedes <alexandredguedes@gmail.com>
2024-09-16 18:03:17 -0300

    form input loop

--- src/app/pages/players/player.routes.ts
+++ src/app/pages/players/player.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { PlayerRouterComponent } from './player-router.component';
+import { PlayerEditComponent } from './player-edit/player-edit.component';
 import { PlayerListComponent } from './player-list/player-list.component';
 
 export const PLAYER_ROUTES: Routes = [
@@ -10,6 +11,10 @@ export const PLAYER_ROUTES: Routes = [
       {
         path: '',
         component: PlayerListComponent,
+      },
+      {
+        path: 'player-edit',
+        component: PlayerEditComponent,
       },
     ],
   },
