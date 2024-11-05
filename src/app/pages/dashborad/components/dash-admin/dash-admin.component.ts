f8a0288d739216546defd697dbc5b22ef89bb74b
aledguedes <alexandredguedes@gmail.com>
2024-11-05 18:34:45 -0300

    primeng prime flex

--- src/app/pages/dashborad/components/dash-admin/dash-admin.component.ts
+++ src/app/pages/dashborad/components/dash-admin/dash-admin.component.ts
@@ -1,31 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { dashAdminMenu } from '../../../../utils/dash-admin-menu';
 import { IMenuDashboard } from '../../../../models/generals/MenuDashboard';
 import { RouterLink } from '@angular/router';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-dash-admin',
   standalone: true,
-  imports: [RouterLink, NgxSkeletonLoaderModule],
+  imports: [RouterLink, ButtonModule, CardModule],
   templateUrl: './dash-admin.component.html',
   styleUrl: './dash-admin.component.scss',
 })
-export class DashAdminComponent implements OnInit {
-  dashMenu: IMenuDashboard[] = [];
-  loader = false;
-  ngOnInit(): void {
-    this.dashMenu = [];
-    setTimeout(() => {
-      this.insertValues(dashAdminMenu);
-    }, 2000);
-
-    console.log('LOADER', this.loader, this.dashMenu, this.dashMenu.length);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  insertValues(data: any) {
-    this.dashMenu = data;
-    this.loader = this.dashMenu.length > 0;
-  }
+export class DashAdminComponent {
+  dashMenu: IMenuDashboard[] = dashAdminMenu;
+  loader = true;
 }
